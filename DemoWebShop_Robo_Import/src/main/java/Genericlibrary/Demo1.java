7ec20244043eb19953608b0b3399517b50cf4cfe
Rakesh B <Rakesh B@TYSS-Rakesh-B>
2024-07-29 11:12:09 +0530

    First Commit

---
+++ DemoWebShop_Robo_Import/src/main/java/Genericlibrary/Demo1.java
@@ -0,0 +1,8 @@
+package Genericlibrary;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ DemoWebShop_Robo_Import/src/test/java/TestScripts/TestScript1.java
@@ -0,0 +1,8 @@
+package TestScripts;
+
+public class TestScript1 {
+
+	public static void main(String[] args) {
+		
+	}
+}
