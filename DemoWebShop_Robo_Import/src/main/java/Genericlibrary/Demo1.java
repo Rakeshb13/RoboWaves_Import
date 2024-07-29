2e52616ceec79cd6d8839fe57114bf03c78c3def
Rakesh B <Rakesh B@TYSS-Rakesh-B>
2024-07-29 11:22:16 +0530

    Updates

--- DemoWebShop_Robo_Import/src/main/java/Genericlibrary/Demo1.java
+++ DemoWebShop_Robo_Import/src/main/java/Genericlibrary/Demo1.java
@@ -3,6 +3,6 @@ package Genericlibrary;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello");
 	}
 }
--- DemoWebShop_Robo_Import/src/test/java/TestScripts/TestScript1.java
+++ DemoWebShop_Robo_Import/src/test/java/TestScripts/TestScript1.java
@@ -4,5 +4,8 @@ public class TestScript1 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Hiii");
+		System.out.println("sdfghjkl;'");
+		
 	}
 }
